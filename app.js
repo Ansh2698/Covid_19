84f94a701ad0ce64e126a46fd086e00810ea8097
Ansh Agrawal <ansh.agrawal.che17@itbhu.ac.in>
2020-04-09 20:52:34 +0530

    Create Home page

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 var express= require("express");
-
+var bodyParser=require("body-parser");
+var http=require("http");
 var app= express();
-app.get("/",(req,res)=>{
-    res.send("We are at home page");
-});
-app.listen(3000);
+var container=require("./container");
+var session=require("express-session");
+container.resolve(function(users){
+    var app=SetExpress();
+    function SetExpress(){
+        var app=express();
+        var server=http.createServer(app);
+        server.listen(3000,function(){
+            console.log("server is running on port 3000");
+        })
+        Configure(app);
+        var router=require("express-promise-router")();
+        users.SetRouting(router);
+        app.use(router);
+    }
+    function Configure(app){
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({ extended: true }));
+    }
+})
